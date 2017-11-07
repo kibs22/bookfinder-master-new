5f5117bc107fb9191077e5f377c686cba06ef435
kibs22 <adriankimdy11@gmail.com>
2017-11-07 20:36:08 +0800

    new
    new

--- public/js/app.js
+++ public/js/app.js
@@ -54382,7 +54382,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
   })]), _vm._v(" "), _c('div', {
     staticClass: "row"
   }, [_c('div', {
-    staticClass: "col-md-3"
+    staticClass: "col-md-2"
   }, [_c('div', {
     staticClass: "card"
   }, [_c('div', {
